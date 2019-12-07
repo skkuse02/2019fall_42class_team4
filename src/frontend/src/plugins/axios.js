e9bf3f2037c14161e80010173f60fa6ba674478c
YoonSeongKyeong <mdy6099@naver.com>
2019-12-08 01:02:24 +0900

    axios req path toggler

--- src/frontend/src/plugins/axios.js
+++ src/frontend/src/plugins/axios.js
@@ -2,4 +2,5 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$http = axios
-Vue.prototype.$http.defaults.baseURL = 'http://34.239.13.251:3000/'
+Vue.prototype.$http.defaults.baseURL = 'http://34.239.13.251:3000/' // release version
+// Vue.prototype.$http.defaults.baseURL = 'http://localhost:3000/' // develop version
