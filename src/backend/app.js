9df268dcbae1078a53d4f055c4eeb2ad4e7d2eff
YoonSeongKyeong <mdy6099@naver.com>
2019-12-07 20:20:13 +0900

    description of cors

--- src/backend/app.js
+++ src/backend/app.js
@@ -23,7 +23,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors())
+app.use(cors())// CORS option can be set with cors module
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
