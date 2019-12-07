f3f97d3223f86dd7b62bc2cd99635862d12c1469
YoonSeongKyeong <mdy6099@naver.com>
2019-12-07 17:47:18 +0900

    release ready

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api',
+        target: 'http://34.239.13.251:3000/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
