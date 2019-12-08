b83f4b0ba12365743229e6310386b3cfbffd220a
YoonSeongKyeong <mdy6099@naver.com>
2019-12-08 20:17:33 +0900

    added mode to test : dev, release

--- src/backend/test/test_review.js
+++ src/backend/test/test_review.js
@@ -7,6 +7,7 @@ chai.use(chaiHttp);
 let crypto = require('crypto');
 
 const baseUrl = 'http://34.239.13.251:3000/api/';
+// const baseUrl = 'http://localhost:3000/api/';
 
 
 async function setuser() {
