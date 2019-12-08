f30fb4471636a15043c906d266eaf14fdda3479e
YoonSeongKyeong <mdy6099@naver.com>
2019-12-08 20:16:20 +0900

    mocha test tool update

--- src/backend/test/test_item.js
+++ src/backend/test/test_item.js
@@ -5,7 +5,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-const baseUrl = 'http://localhost:3000/api/';
+const baseUrl = 'http://34.239.13.251:3000/api/';
 
 async function items() {
 	url = baseUrl + 'items';
